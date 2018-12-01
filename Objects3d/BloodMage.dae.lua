9aa1ddae297db66b2392aa432a8acf4e8942e203
Gajo Petrovic <gajo01@gmail.com>
2018-12-01 16:47:28 +0900

    add textures

---
+++ Objects3d/BloodMage.dae.lua
@@ -0,0 +1,7 @@
+model = {
+      radius = 90.0,
+      height = 90.0,
+      tex1 = "BloodMageLPTexture.png",
+      midpos = {0,30,0},
+}
+return model
---
+++ Objects3d/HumanWithStick.dae.lua
@@ -0,0 +1,7 @@
+model = {
+      radius = 90.0,
+      height = 90.0,
+      tex1 = "HumanWithStickLPTexture.png",
+      midpos = {0,30,0},
+}
+return model
