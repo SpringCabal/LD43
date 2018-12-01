6ea7cfbc3f7fa9339766332a7b2d2f6efafb665c
Gajo Petrovic <gajo01@gmail.com>
2018-12-01 15:24:30 +0900

    fix house defs

--- Units/buildings.lua
+++ Units/buildings.lua
@@ -1,20 +1,22 @@
 local House = Unit:New {
 	buildPic            = "",
 
-	acceleration        = 0.5,
-	brakeRate           = 0.4,
     --buildCostMetal        = 65, -- used only for power XP calcs
     canMove             = false,
+    maxVelocity         = 0,
 --     canGuard            = false,
 --     canPatrol           = false,
 --     canRepeat           = false,
 
     --pushResistant       = true,
+
     collisionVolumeScales   = '37 40 37',
     collisionVolumeTest     = 1,
     collisionVolumeType     = 'CylY',
     footprintX          = 6,
     footprintZ          = 6,
+    yardmap = "oooooooooooooooooooooooooooooooooooo",
+
     mass                = 50,
     minCollisionSpeed   = 1,
 
@@ -23,21 +25,18 @@ local House = Unit:New {
 
 
     stealth             = true,
-    turnRate            = 3000,
     upright             = true,
 
 
     name                = "House",
     activateWhenBuilt   = true,
-    customParams = {
-    },
+    customParams = {   },
 
     idletime					= 120, --in simframes
     idleautoheal 				= 50,
     autoheal 					= 1,
 
     maxDamage           = 1600,
-    maxVelocity         = 10,
     onoffable           = true,
     script              = "house.lua",
 	objectName 			= "buildingLP.dae",
