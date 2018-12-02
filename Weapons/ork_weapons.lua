248d2800cf18dfa77253dbb4a223582aa91f4eaa
GoogleFrog <googl3frog@gmail.com>
2018-12-03 01:42:23 +1100

    Small orks destroy houses.

--- Weapons/ork_weapons.lua
+++ Weapons/ork_weapons.lua
@@ -39,39 +39,41 @@ local Bite = MeleeWeapon:New {
 }
 
 local Firebomb = Weapon:New {
-	--name                  = "Firebomb",
-	--range                 = 250,
-	--reloadTime            = 15,
-	---- targeting & accuracy
-	---- general
-	--weaponType            = "Cannon", -- there's a default, but honestly, why?
-	--impactOnly            = true,
-	--noSelfDamage          = true,
-	--weaponVelocity        = 500,
-	--myGravity             = 1,
-	--tolerance             = 6000,
-	--turret                = true,
-	-- -- collision & avoidance
-	--avoidFriendly         = false,
-	--avoidFeature          = false,
-	-- collideFriendly      = false,
-	--collideFeature        = false,
-	--
+	name                  = "Firebomb",
+	range                 = 350,
+	reloadTime            = 15,
+	-- targeting & accuracy
+	-- general
+	weaponType            = "Cannon", -- there's a default, but honestly, why?
+	impactOnly            = true,
+	noSelfDamage          = true,
+	weaponVelocity        = 550,
+	myGravity             = 1.2,
+	tolerance             = 6000,
+	highTrajectory        = 1,
+	turret                = true,
+	 -- collision & avoidance
+	avoidFriendly         = false,
+	avoidFeature          = false,
+	 collideFriendly      = false,
+	collideFeature        = false,
+	
+	-- If you uncomment this, then both orksmall and orkbig will lose the ability to attack.
 	--customParams = {
 	--	light_radius = 350,
 	--	light_fade_time = 1,
 	--	light_fade_offset = 10,
 	--}
-	--
-	-- impulseFactor = 0,
-	-- -- targeting & accuracy
-	--accuracy              = 0.1,
-	---- model                 = 'spear.dae',
-	-- -- soundStart            = [[SpearThrow]],
-	--soundHit              = [[Hit]],
-	-- damage                = {
-	--	default = 600,
-	--}
+	
+	 impulseFactor = 0,
+	 -- targeting & accuracy
+	accuracy              = 0.1,
+	-- model                 = 'spear.dae',
+	 -- soundStart            = [[SpearThrow]],
+	soundHit              = [[Hit]],
+	 damage                = {
+		default = 600,
+	}
 }
 
 
