6256e3ea3a3f533bf61a631ffcd56f292534c332
Gajo Petrovic <gajo01@gmail.com>
2018-12-02 13:24:53 +0900

    less spamy unitscripts

--- scripts/ork.lua
+++ scripts/ork.lua
@@ -1,11 +1,9 @@
 local handRight = piece("Hand_Right")
 
 function script.QueryWeapon()
-	Spring.Echo("QUERY")
 	return handRight
 end
 
 function script.AimWeapon()
-	Spring.Echo("AIM")
 	return true
 end
