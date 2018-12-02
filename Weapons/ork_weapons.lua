0852db9ff778127ddfbec0369e3631416e52856c
GoogleFrog <googl3frog@gmail.com>
2018-12-03 01:35:54 +1100

    Fixed previous. Orks can now attack again.

--- Weapons/ork_weapons.lua
+++ Weapons/ork_weapons.lua
@@ -39,39 +39,39 @@ local Bite = MeleeWeapon:New {
 }
 
 local Firebomb = Weapon:New {
-	name                  = "Firebomb",
-	range                 = 250,
-	reloadTime            = 15,
-	-- targeting & accuracy
-	-- general
-	weaponType            = "Cannon", -- there's a default, but honestly, why?
-	impactOnly            = true,
-	noSelfDamage          = true,
-	weaponVelocity        = 500,
-	myGravity             = 1,
-	tolerance             = 6000,
-	turret                = true,
-	 -- collision & avoidance
-	avoidFriendly         = false,
-	avoidFeature          = false,
-	 collideFriendly      = false,
-	collideFeature        = false,
-	
-	customParams = {
-		light_radius = 350,
-		light_fade_time = 1,
-		light_fade_offset = 10,
-	}
-	
-	 impulseFactor = 0,
-	 -- targeting & accuracy
-	accuracy              = 0.1,
-	-- model                 = 'spear.dae',
-	 -- soundStart            = [[SpearThrow]],
-	soundHit              = [[Hit]],
-	 damage                = {
-		default = 600,
-	}
+	--name                  = "Firebomb",
+	--range                 = 250,
+	--reloadTime            = 15,
+	---- targeting & accuracy
+	---- general
+	--weaponType            = "Cannon", -- there's a default, but honestly, why?
+	--impactOnly            = true,
+	--noSelfDamage          = true,
+	--weaponVelocity        = 500,
+	--myGravity             = 1,
+	--tolerance             = 6000,
+	--turret                = true,
+	-- -- collision & avoidance
+	--avoidFriendly         = false,
+	--avoidFeature          = false,
+	-- collideFriendly      = false,
+	--collideFeature        = false,
+	--
+	--customParams = {
+	--	light_radius = 350,
+	--	light_fade_time = 1,
+	--	light_fade_offset = 10,
+	--}
+	--
+	-- impulseFactor = 0,
+	-- -- targeting & accuracy
+	--accuracy              = 0.1,
+	---- model                 = 'spear.dae',
+	-- -- soundStart            = [[SpearThrow]],
+	--soundHit              = [[Hit]],
+	-- damage                = {
+	--	default = 600,
+	--}
 }
 
 
