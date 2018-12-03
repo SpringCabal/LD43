e5097071d8e3c473abbafc1202b4d0796f0223cd
GoogleFrog <googl3frog@gmail.com>
2018-12-03 14:47:49 +1100

    Death animation for house.

--- scripts/house.lua
+++ scripts/house.lua
@@ -1,6 +1,16 @@
+local House = piece("House")
 
 function script.HitByWeapon(x, z, weaponID, damage)
 	if damage > 400 then
 		Spring.SetUnitRulesParam(unitID, "on_fire", 1)
 	end
+end
+
+function script.Killed(recentDamage, maxHealth)
+	Move(House, z_axis, -500, 700)
+	Turn(House, x_axis, math.random() - 0.5, math.random())
+	Turn(House, y_axis, math.random() - 0.5, math.random())
+	Turn(House, z_axis, (math.random() - 0.5)/2, math.random()/2)
+	Sleep(1000)
+	return 0
 end
